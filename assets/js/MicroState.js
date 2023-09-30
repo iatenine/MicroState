2e6148e0c94a50b03a6bd11b151cd69140efa154
iatenine <iatenine@gmail.com>
2023-09-30 15:31:56 -0500

    sanitize HTML strings from state

--- assets/js/MicroState.js
+++ assets/js/MicroState.js
@@ -30,7 +30,7 @@ class MicroState {
    */
   setState(state) {
     const prevState = { ...this.state };
-    this.state = state;
+    this.state = MicroState.escapeHTML(state);
     this._render(prevState);
   }
   /**
@@ -79,5 +79,29 @@ class MicroState {
     this.onBeforeRender(newState, prevState);
     this.mountPoint.innerHTML = this.root(newState, prevState);
     this.onAfterRender(newState, prevState);
+  }
+
+  static escapeHTML(state) {
+    // recursively escapeHTML of all strings in state
+    const keys = Object.keys(state);
+    for (const key of keys) {
+      const value = state[key];
+      if (typeof value === "object" || typeof value === "array") {
+        state[key] = MicroState.escapeHTML(value);
+      }
+      if (typeof value === "string") {
+        state[key] = MicroState.sanitizeHTML(value);
+      }
+    }
+    return state;
+  }
+
+  static sanitizeHTML(unsafe) {
+    return unsafe
+      .replace(/&(?!(amp;|lt;|gt;|#39;|quot;))/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/'/g, "&#39;")
+      .replace(/"/g, "&quot;");
   }
 }
